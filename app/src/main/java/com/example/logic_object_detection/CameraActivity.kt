02b125ae26e4c499a558a57846adc2567b8737c6
Timmy.Hsieh <timmy.hsieh@lig.com.tw>
2023-02-03 17:49:39 +0800

    可以work第一版

--- app/src/main/java/com/example/logic_object_detection/CameraActivity.kt
+++ app/src/main/java/com/example/logic_object_detection/CameraActivity.kt
@@ -1,22 +1,25 @@
 package com.example.logic_object_detection
 
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.view.*
-import android.widget.Toolbar
-import androidx.appcompat.app.ActionBar
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.logic_object_detection.databinding.ActivityCameraBinding
-import com.google.ar.core.Frame
-import com.google.ar.sceneform.FrameTime
-import com.google.ar.sceneform.Scene
-import org.tensorflow.lite.InterpreterApi
 
 
-class CameraActivity : AppCompatActivity(),Scene.OnUpdateListener {
-    private lateinit var binding:ActivityCameraBinding
-    private lateinit var arFragment:CustomArFragment
+class CameraActivity : AppCompatActivity()/*, Scene.OnUpdateListener */{
+    companion object{
+        const val TARGET_IMAGE_URI ="TARGET_IMAGE_URI"
+    }
+    private lateinit var binding: ActivityCameraBinding
+    private lateinit var arFragment: CustomArFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -24,11 +27,31 @@ class CameraActivity : AppCompatActivity(),Scene.OnUpdateListener {
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(binding.root)
         supportActionBar!!.hide()
-        arFragment=supportFragmentManager.findFragmentById(R.id.ArFagment) as CustomArFragment
-        arFragment.arSceneView.scene.addOnUpdateListener(this)
+        arFragment =
+            (supportFragmentManager.findFragmentById(R.id.ArFagment) as CustomArFragment).apply {
+                (intent.getParcelableExtra(TARGET_IMAGE_URI) as? Uri)?.let { uri ->
+                    targetBitmap =BitmapFactory.decodeStream(contentResolver.openInputStream(uri)).compress(1f).apply{
+                        Log.e("CameraActivity", "完成的Bitmap是=>${this}")
+                        Log.e("CameraActivity", "完成的Bitmap的大小是=>[${this.height},${this.width}]")
+                        Log.e("CameraActivity", "完成的Bitmap的密度是=>[${this.density}]")
+                    }
+                }
+            }
+//        arFragment.arSceneView.scene.addOnUpdateListener(this)
     }
-    override fun onUpdate(frameTime: FrameTime?) {
-        val frame: Frame=arFragment.arSceneView.arFrame!!
+
+//    override fun onUpdate(frameTime: FrameTime?) {
+//        val frame: Frame = arFragment.arSceneView.arFrame!!
+//    }
+
+    private fun Bitmap.compress(scale: Float): Bitmap {
+        // Use matrix 改變圖片長、寬
+        val matrix = Matrix()
+        // 縮小的比例
+        matrix.setScale(scale, scale)
+        val after = Bitmap.createBitmap(this, 0, 0, this.width, this.height, matrix, true)
+//        this.recycle() //不可以回收，若回收的話會導致ArSceneActivity Destroy的時候失敗。
+        return after
     }
 
 }
